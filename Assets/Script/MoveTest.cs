58f9c9904aefe4d27fd69a30f4706c7efb297fcd
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-15 15:13:33 +0700

    -update funcction move

--- Assets/Script/MoveTest.cs
+++ Assets/Script/MoveTest.cs
@@ -7,30 +7,37 @@ public class MoveTest : MonoBehaviour
 
     public GameObject cube1;
     public float speedCube2 = 1f;
-
     private Rigidbody rb;
+    private bool checkCLick = false;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = cube1.GetComponent<Rigidbody>();
     }
 
-    private void OnMouseDown()
+    void CubeMove()
     {
         if(this.name == "Cube2"){
-            rb.MovePosition(this.transform.position);
+            rb.velocity = this.transform.position - cube1.transform.position;
+        }
+        if(this.name == "Cube3"){
+            rb.velocity = this.transform.position - cube1.transform.position;
         }
     }
 
-    private void OnMouseDrag()
-    {
-        if(this.name == "Cube3"){
-            cube1.transform.position = Vector3.MoveTowards(cube1.transform.position, this.transform.position, speedCube2);
-        }
+    void OnMouseDown() {
+        checkCLick = true;
+    }
+
+    void OnMouseUp() {
+        checkCLick = false;
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(checkCLick == true){
+            CubeMove();
+        }
     }
 }
